4ecc0c59cb921ee1ceb42d33ee9118177891a178
eLn86 <elnathan.erh@gmail.com>
2018-04-19 11:33:23 +0800

    merged sendEmail with fetch data, added verify email logic

--- api/dashboard.js
+++ api/dashboard.js
@@ -1,7 +1,9 @@
 'use strict';
 const fetch = require('node-fetch')
+const aws = require('aws-sdk')
+const ses = new aws.SES({ region: 'us-east-1' })
 
-module.exports.fetchTopUsageData = (event, context, callback) => {
+module.exports.fetchTopUsageDataAndSendEmail = (event, context, callback) => {
   fetch('https://api.ci.palo-it-hk.com/usages/top-usage/5/type/total/daterange/20170410/20170411')
     .then(response => {
       if (response.status !== 200) {
@@ -13,7 +15,71 @@ module.exports.fetchTopUsageData = (event, context, callback) => {
           statusCode: 200,
           data: json
         }
-        callback(null, result)
+        // callback(null, JSON.stringify(result.data))
+        const dataToSend = 
+        [
+          `<table>
+            <tr>
+              <th>Station Name</th>
+              <th>District</th>
+              <th>Total Bikes Count</th>
+              <th>Total Bikes Out</th>
+              <th>Total Bikes In</th>
+            </tr>`,
+            result.data.map(station =>   
+              `
+              <tr>
+                <td>${station.location}</td>
+                <td>${station.district}</td>
+                <td>${station.totalBikesCount}</td>
+                <td>${station.totalBikesOut}</td>
+                <td>${station.totalBikesIn}</td>
+              </tr>
+              `
+            ).join(''),
+            `</table>`
+        ].join('')
+        
+        // Define Email Params
+        let params = {
+          Destination: {
+            ToAddresses: [
+              "elnathan.erh@gmail.com",
+            ]
+          }, 
+          Message: {
+            Body: {
+              Html: {
+                Data: dataToSend,
+                Charset: "UTF-8"
+              },
+              Text: {
+                Data: dataToSend,
+                Charset: "UTF-8"
+              }
+            }, 
+            Subject: {
+              Data: "Top 5 Stations in London for today",
+              Charset: "UTF-8"
+            }
+          }, 
+          Source: "eerh@palo-it.com", 
+          Tags: [
+            {
+              Name: 'Send_individual_email',
+              Value: 'email1'
+            }
+          ]
+        }
+        
+        // Send the email
+        ses.sendEmail(params, (err, data) => {
+          if (err) console.log(err)
+          else { 
+            context.succeed(data.msg)
+            callback(null, 'Email Sent Successfully')
+          }
+        })
       })
     })
     .catch(err => {
---
+++ api/verifyEmail.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk')
+const ses = new aws.SES({ region: 'us-east-1' })
+
+module.exports.handler = (event, context, callback) => {
+  let identityParams = {
+      IdentityType: "EmailAddress", 
+      MaxItems: 123, 
+      NextToken: ""
+  }
+
+  // List all the identities in SES
+  ses.listIdentities(identityParams, (err, data) => {
+    if (err) console.log(err, err.stack)
+    else {
+      // Check if email passed from frontend already exists
+      const identitiesArray = data.Identities
+      const emailAddress = event.email
+      const filteredResult = identitiesArray.filter(email => email === emailAddress)
+      
+      // If email does not exist in SES, send to SES for verification
+      if(filteredResult.length === 0) {
+        let params = {
+          EmailAddress: event.email
+        }
+        ses.verifyEmailIdentity(params, (err, data) => {
+          if (err) console.log(err, err.stack)
+          else     context.succeed(data)    
+        })
+      }
+    }
+  })
+}
