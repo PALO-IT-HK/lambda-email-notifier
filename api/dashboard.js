ac887d4f21ec6014e46879c8033048ad5b4ac172
Beda Tse <beda@coeus.hk>
2018-04-20 11:03:40 +0800

    send email update

--- api/dashboard.js
+++ api/dashboard.js
@@ -6,6 +6,13 @@ const aws = require('aws-sdk');
 const ses = new aws.SES({ region: 'us-east-1' });
 const lambda = new aws.Lambda();
 
+const corsHeaders = {
+  'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Allow-Methods': 'POST',
+};
+
 function listSESIdentities() {
   return new Promise((resolve, reject) => {
     ses.listIdentities({
@@ -28,9 +35,9 @@ function getSESIdentifyVerificationAttributes(emails) {
       Identities: emails,
     }, (err, data) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(data)
+        resolve(data);
       }
     });
   });
@@ -42,12 +49,41 @@ function sendEmailLambda(functionName, payload) {
       FunctionName: functionName, // the lambda function we are going to invoke
       InvocationType: 'RequestResponse',
       LogType: 'Tail',
-      Payload: JSON.stringify(payload)
+      Payload: JSON.stringify(payload),
     }, (err, data) => {
-      if(err) reject(err)
-      else resolve(data)
-    })
-  })
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+function buildEmail(data) {
+  const emailObject = {
+    dataToSend:
+      [
+        `<table>
+                    <tr>
+                      <th>Station Name</th>
+                      <th>District</th>
+                      <th>Total Bikes Count</th>
+                      <th>Total Bikes Out</th>
+                      <th>Total Bikes In</th>
+                    </tr>`,
+        data.map(station =>
+          `
+                      <tr>
+                        <td>${station.location}</td>
+                        <td>${station.district}</td>
+                        <td>${station.totalBikesCount}</td>
+                        <td>${station.totalBikesOut}</td>
+                        <td>${station.totalBikesIn}</td>
+                      </tr>
+                      `).join(''),
+        '</table>',
+      ].join(''),
+  };
+
+  return emailObject;
 }
 
 module.exports.fetchTopUsageDataAndSendEmail = (event, context, callback) => {
@@ -64,6 +100,7 @@ module.exports.fetchTopUsageDataAndSendEmail = (event, context, callback) => {
           }),
         });
       }
+
       return response.json().then((json) => {
         const result = {
           statusCode: 200,
@@ -72,168 +109,140 @@ module.exports.fetchTopUsageDataAndSendEmail = (event, context, callback) => {
 
         // Get the list of identities from SES
         listSESIdentities()
-          .then(data => {
-            getSESIdentifyVerificationAttributes(data.Identities)
-              .then(data => {
-                const resultsObj = data.VerificationAttributes
-                let updatedArray = []
-                  for(let key in resultsObj) {
-                   updatedArray.push({
-                     email: key,
-                     status: resultsObj[key].VerificationStatus
-                   })
-                  }
-                return updatedArray.filter(item => item.status !== 'Pending' && item.email !== 'eerh@palo-it.com')
-              })
-              .then(emailsArray => {
-                // Create email object
-                const emailObject = {
-                  dataToSend:         
-                    [
-                      `<table>
-                        <tr>
-                          <th>Station Name</th>
-                          <th>District</th>
-                          <th>Total Bikes Count</th>
-                          <th>Total Bikes Out</th>
-                          <th>Total Bikes In</th>
-                        </tr>`,
-                      emailsArray.map(station =>
-                        `
-                          <tr>
-                            <td>${station.location}</td>
-                            <td>${station.district}</td>
-                            <td>${station.totalBikesCount}</td>
-                            <td>${station.totalBikesOut}</td>
-                            <td>${station.totalBikesIn}</td>
-                          </tr>
-                          `).join(''),
-                      '</table>',
-                    ].join(''),
-                }
-
-                // Send individual email for each email in array
-                emailsArray.forEach(item => {
-                  const payload = {
-                    dataToSend: emailObject.dataToSend,
-                    email: item.email
-                  }
-                  sendEmailLambda('dashboard-dev-sendEmail', payload)
-                    .then(data => {
-                      callback(null, {
-                        statusCode: 200,
-                        headers: corsHeaders,
-                        body: JSON.stringify({
-                          status: 'accepted',
-                          msg: 'Call sendEmail lambda function success!'
-                        })
-                      })
-                    })
-                    .catch(err => {
-                      callback(null, {
-                        statusCode: 500,
-                        body: JSON.stringify({
-                          status: 'failed',
-                          msg: 'Call sendEmail lambda function failed!'
-                        })
-                      })
-                    })
-                })
-              })
-                
-
-    
-                
-
+          .then(data => getSESIdentifyVerificationAttributes(data.Identities))
+          .then((data /* getSESIdentifyVerificationAttributes */) => {
+            const resultsObj = data.VerificationAttributes;
+            const updatedArray = [];
+            for (const key in resultsObj) { // eslint-disable-line
+              updatedArray.push({
+                email: key,
+                status: resultsObj[key].VerificationStatus,
+              });
+            }
+            return updatedArray.filter(item => item.status !== 'Pending' && item.email !== 'eerh@palo-it.com');
           })
-        
-        //     })
-        //   })
-        //   .catch(err => {
-        //     console.log(err)
-        //     callback(null, {
-        //       statusCode: 500,
-        //       headers: corsHeaders,
-        //       body: JSON.stringify({
-        //         status: 'failed',
-        //         msg: 'Unknown error occured',
-        //       })
-        //     })
-        //   }) // closing for catch function
-        // })
-      }) // .then(emailsArray => {
-    })
-  }
-            // // Filter source email (eerh@palo-it.com) out from the list
-            // const filteredEmailList = data.Identities.filter(email => email !== 'eerh@palo-it.com')
-            // const emailObject = {
-            //   dataToSend:         
-            //     [
-            //       `<table>
-            //         <tr>
-            //           <th>Station Name</th>
-            //           <th>District</th>
-            //           <th>Total Bikes Count</th>
-            //           <th>Total Bikes Out</th>
-            //           <th>Total Bikes In</th>
-            //         </tr>`,
-            //       result.data.map(station =>
-            //         `
-            //           <tr>
-            //             <td>${station.location}</td>
-            //             <td>${station.district}</td>
-            //             <td>${station.totalBikesCount}</td>
-            //             <td>${station.totalBikesOut}</td>
-            //             <td>${station.totalBikesIn}</td>
-            //           </tr>
-            //           `).join(''),
-            //       '</table>',
-            //     ].join(''),
-            // }
-            // filteredEmailList.forEach(email => {
-            //   const payload = {
-            //     dataToSend: emailObject.dataToSend,
-            //     email: email
-            //   }
-            //   const sendEmailLambdaParams = {
-            //     FunctionName: 'dashboard-dev-sendEmail', // the lambda function we are going to invoke
-            //     InvocationType: 'RequestResponse',
-            //     LogType: 'Tail',
-            //     Payload: JSON.stringify(payload)
-            //   }
-            
-              // lambda.invoke(sendEmailLambdaParams, (err, data) => {
-              //   if(err) {
-              //     return callback(null, {
-              //       statusCode: 500,
-              //       body: JSON.stringify({
-              //         status: 'failed',
-              //         msg: 'Invoke sendEmail Lambda error: ' + err
-              //       })
-              //     })
-              //   }
-              //   else {
-              //     return callback(null, {
-              //       statusCode: 200,
-              //       body: JSON.stringify({
-              //         status: 'success',
-              //         msg: 'Invoke sendEmail Lambda successful'
-              //       })
-              //     })
-              //   }
-              // }) // lambda.invoke closing brackets
-    //         }) // forEach closing brackets
-    //       }
-    //     });
-    //   });
-    // })
-    // .catch((err) => {
-    //   console.log(`Error: ${err}`);
-    //   return callback(null, {
-    //     statusCode: 500,
-    //     body: JSON.stringify({
-    //       status: 'failed',
-    //       msg: 'unknown error occurred',
-    //     }),
-    //   });
-    // });
+          .then((emailsArray) => {
+            // Create email object
+            const emailObject = buildEmail(result.data);
+
+            // Send individual email for each email in array
+            emailsArray.forEach((item) => {
+              const payload = {
+                dataToSend: emailObject.dataToSend,
+                email: item.email,
+              };
+              sendEmailLambda('dashboard-dev-sendEmail', payload)
+                .then((data) => {
+                  console.log({msg: 'sendEmailLambda Invoke Success', data});
+                  callback(null, {
+                    statusCode: 200,
+                    headers: corsHeaders,
+                    body: JSON.stringify({
+                      status: 'accepted',
+                      msg: 'Call sendEmail lambda function success!',
+                    }),
+                  });
+                })
+                .catch((err) => {
+                  console.log({msg: 'sendEmailLambda Invoke failed', err});
+                  callback(null, {
+                    statusCode: 500,
+                    body: JSON.stringify({
+                      status: 'failed',
+                      msg: 'Call sendEmail lambda function failed!',
+                    }),
+                  });
+                });
+            });
+          });
+      });
+
+      //     })
+      //   })
+      //   .catch(err => {
+      //     console.log(err)
+      //     callback(null, {
+      //       statusCode: 500,
+      //       headers: corsHeaders,
+      //       body: JSON.stringify({
+      //         status: 'failed',
+      //         msg: 'Unknown error occured',
+      //       })
+      //     })
+      //   }) // closing for catch function
+      // })
+    }); // .then(emailsArray => {
+};
+// // Filter source email (eerh@palo-it.com) out from the list
+// const filteredEmailList = data.Identities.filter(email => email !== 'eerh@palo-it.com')
+// const emailObject = {
+//   dataToSend:
+//     [
+//       `<table>
+//         <tr>
+//           <th>Station Name</th>
+//           <th>District</th>
+//           <th>Total Bikes Count</th>
+//           <th>Total Bikes Out</th>
+//           <th>Total Bikes In</th>
+//         </tr>`,
+//       result.data.map(station =>
+//         `
+//           <tr>
+//             <td>${station.location}</td>
+//             <td>${station.district}</td>
+//             <td>${station.totalBikesCount}</td>
+//             <td>${station.totalBikesOut}</td>
+//             <td>${station.totalBikesIn}</td>
+//           </tr>
+//           `).join(''),
+//       '</table>',
+//     ].join(''),
+// }
+// filteredEmailList.forEach(email => {
+//   const payload = {
+//     dataToSend: emailObject.dataToSend,
+//     email: email
+//   }
+//   const sendEmailLambdaParams = {
+//     FunctionName: 'dashboard-dev-sendEmail', // the lambda function we are going to invoke
+//     InvocationType: 'RequestResponse',
+//     LogType: 'Tail',
+//     Payload: JSON.stringify(payload)
+//   }
+
+// lambda.invoke(sendEmailLambdaParams, (err, data) => {
+//   if(err) {
+//     return callback(null, {
+//       statusCode: 500,
+//       body: JSON.stringify({
+//         status: 'failed',
+//         msg: 'Invoke sendEmail Lambda error: ' + err
+//       })
+//     })
+//   }
+//   else {
+//     return callback(null, {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         status: 'success',
+//         msg: 'Invoke sendEmail Lambda successful'
+//       })
+//     })
+//   }
+// }) // lambda.invoke closing brackets
+//         }) // forEach closing brackets
+//       }
+//     });
+//   });
+// })
+// .catch((err) => {
+//   console.log(`Error: ${err}`);
+//   return callback(null, {
+//     statusCode: 500,
+//     body: JSON.stringify({
+//       status: 'failed',
+//       msg: 'unknown error occurred',
+//     }),
+//   });
+// });
--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,63 +1,66 @@
 const aws = require('aws-sdk');
+
 const ses = new aws.SES({ region: 'us-east-1' });
 
 function sesSendEmail(eventObject) {
   return new Promise((resolve, reject) => {
-    ses.sendEmail({
-      Destination: {
-        ToAddresses: [
-          eventObject.email
-        ],
-      },
-      Message: {
-        Body: {
-          Html: {
-            Data: eventObject.dataToSend,
-            Charset: 'UTF-8'
+    ses.sendEmail(
+      {
+        Destination: {
+          ToAddresses: [
+            eventObject.email,
+          ],
+        },
+        Message: {
+          Body: {
+            Html: {
+              Data: eventObject.dataToSend,
+              Charset: 'UTF-8',
+            },
+            Text: {
+              Data: eventObject.dataToSend,
+              Charset: 'UTF-8',
+            },
           },
-          Text: {
-            Data: dataToSend,
+          Subject: {
+            Data: 'Top 5 Stations in London for today',
             Charset: 'UTF-8',
           },
         },
-        Subject: {
-          Data: 'Top 5 Stations in London for today',
-          Charset: 'UTF-8',
-        },
+        Source: 'eerh@palo-it.com',
+        Tags: [
+          {
+            Name: 'Send_individual_email',
+            Value: 'email1',
+          },
+        ],
       },
-      Source: 'eerh@palo-it.com',
-      Tags: [
-        {
-          Name: 'Send_individual_email',
-          Value: 'email1',
-        },
-      ]
-    }),
       (err, data) => {
-        err ? reject(err) : resolve(data)
-      } 
-  })
+        err ? reject(err) : resolve(data);
+      },
+    );
+  });
 }
 
 module.exports.handler = (event, context, callback) => {
   sesSendEmail(event)
-    .then(data => {
+    .then((data) => {
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({
           status: 'success',
-          msg: 'Email Sent Successfully'
-        })
-      })
+          msg: 'Email Sent Successfully',
+        }),
+      });
     })
-    .catch(err => {
-      console.log(err)
+    .catch((err) => {
+      console.log(err);
       callback(null, {
         statusCode: 500,
         body: JSON.stringify({
           status: 'failed',
-          msg: 'Error in Sending Email: ' + err 
+          msg: `Error in Sending Email: ${err}`,
         }),
-      })
-    })
-}
+      });
+    });
+};
